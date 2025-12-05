525ef1f4acdc71820f22c5defd448bb521f78edf
Marat Akbay <maratakbaj@gmail.com>
2025-12-05 14:07:46 +0500

    Init Spring Boot

---
+++ backend/src/main/java/com/oquga/oquga/OqugaApplication.java
@@ -0,0 +1,13 @@
+package com.oquga.oquga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OqugaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OqugaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/oquga/oquga/OqugaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oquga.oquga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OqugaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
