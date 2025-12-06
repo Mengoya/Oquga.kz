4e43482c2dfc40905ae42d9d9b6f9d93d9a369e8
Marat Akbay <maratakbaj@gmail.com>
2025-12-06 12:00:54 +0500

    mjs file added

---
+++ frontend-client/next.config.mjs
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
+
+const nextConfig = {
+    output: 'standalone',
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 's3.oquga.kz',
+            },
+            {
+                protocol: 'https',
+                hostname: 'avatar.vercel.sh',
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+            },
+        ],
+        unoptimized: false,
+    },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '2mb',
+        },
+    },
+};
+
+export default withNextIntl(nextConfig);
